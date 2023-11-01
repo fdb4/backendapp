84196b2d3bb68cce8ac17bcee3591519e51d26bc
FrancescoDB <fdb4@njit.edu>
2023-11-01 13:58:33 -0400

    Added files

--- backend/config/models.py
+++ backend/config/models.py
@@ -1,12 +1,28 @@
-from flask_sqlalchemy import SQLAlchemy
+from exts import db
 from uuid import uuid1
-db=SQLAlchemy()
-class Clients(db.model):
-    __table__="clients"
-    client_id=db.Column(db.Integer,primary_key=True)
-    email=db.Column(db.String(45))
-    password=db.Column(db.String(45))
-    coachID=db.Column(db.Integer)
-    firstname=db.Column(db.String(45))
-    lastname = db.Column(db.String(45))
 
+
+class Clients(db.Model):
+    client_id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(45), nullable=False)
+    password = db.Column(db.String(45), nullable=False)
+    coachID = db.Column(db.Integer, nullable=False)
+    firstname = db.Column(db.String(45), nullable=False)
+    lastname = db.Column(db.String(45), nullable=False)
+
+    def __repr__(self):
+        return f"<Clients (self.title) >"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self, email, password, firstname, lastname):
+        self.email = email
+        self.password = password
+        self.firstname = firstname
+        self.lastname = lastname
