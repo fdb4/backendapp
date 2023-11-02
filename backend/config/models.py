602994d620734ed5cbc96f5fb505665627d45116
Gianna <gbauzil02@gmail.com>
2023-11-02 17:46:53 -0400

    Changing the column attributes

--- backend/config/models.py
+++ backend/config/models.py
@@ -1,28 +1,31 @@
 from exts import db
 from uuid import uuid1
 
-
 class Clients(db.Model):
-    client_id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(45), nullable=False)
-    password = db.Column(db.String(45), nullable=False)
-    coachID = db.Column(db.Integer)
-    firstname = db.Column(db.String(45), nullable=False)
+    __tablename__="Clients"
+    client_id=db.Column(db.Integer,primary_key=True, autoincrement=True)
+    email=db.Column(db.String(45), nullable=False, unique=True)
+    password=db.Column(db.String(128), nullable=False)
+    coachID=db.Column(db.Integer)
+    firstname=db.Column(db.String(45), nullable=False)
     lastname = db.Column(db.String(45), nullable=False)
 
     def __repr__(self):
-        return f"<Clients (self.title) >"
-
+        return f"<Clients (self.firstname) >"
+    
     def save(self):
         db.session.add(self)
         db.session.commit()
-
+    
     def delete(self):
         db.session.delete(self)
         db.session.commit()
 
-    def update(self, email, password, firstname, lastname):
+    def update(self,email, password, firstname, lastname):
         self.email = email
-        self.password = password
+        self.pword = password
         self.firstname = firstname
         self.lastname = lastname
+
+        db.session.commit()
+
