533e8288a6ea218f95e873a1636e5ea542977cc1
Gianna <gbauzil02@gmail.com>
2023-11-09 13:13:00 -0500

    Adding GeneralInfo Model

--- backend/data/models.py
+++ backend/data/models.py
@@ -27,4 +27,26 @@ class Clients(db.Model):
         self.lastname = lastname
 
         db.session.commit()
+    
+class GeneralInfo(db.Model):
+    __tablename__="generalinfo"
+    geninfoID=db.Column(db.Integer,primary_key=True, unique=True)
+    height=db.Column(db.Double, nullable=False, unique=True)
+    weight=db.Column(db.Double, nullable=False)
+    goalweight=db.Column(db.Integer, nullable=False)
+    movement = db.Column(db.String(45), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.Integer, nullable=False)
+    def __repr__(self):
+        return f"<GeneralInfo (self.firstname) >"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
 
