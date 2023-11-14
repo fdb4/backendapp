064c1b2c340a0abc3b4723e851c62fb74f961a20
Gianna <gbauzil02@gmail.com>
2023-11-14 18:45:32 -0500

    Adding CORS

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
 from data.exts import db
 from data.models import Clients, GeneralInfo, WorkoutGoal, CoachExp, Location, State, ClientCoaches
 from config.config import DevConfig
 
 app=Flask(__name__)
+CORS(app)
 app.config.from_object(DevConfig)
 db.init_app(app)
 api=Api(app,doc='/docs')
