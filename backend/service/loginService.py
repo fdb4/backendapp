4f80f3719d4a54ad9b0e6f5939e90d193fcedf12
Gianna <gbauzil02@gmail.com>
2023-11-15 16:14:35 -0500

    fixing login

--- backend/service/loginService.py
+++ backend/service/loginService.py
@@ -6,8 +6,8 @@ from flask import jsonify, session
 def loginClient(email,password):
         client= Clients.query.filter_by(email=email).first()
         if client is None:
-            return jsonify({"message":"Wrong email/username"}),401
+            return {"message":"Wrong email/username"},401
         if not check_password_hash(client.password,password):
-            return jsonify({"message":"Wrong email/username"}),401
+            return {"message":"Wrong password"},401
         session["clientID"]=client.clientID
         return jsonify({"message":"Success"})
