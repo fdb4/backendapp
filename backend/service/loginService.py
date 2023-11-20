f14e38faf9e6dd9316d40e0ec2e7d7e217e2910e
Gianna Bauzil <73215280+gbauzil02@users.noreply.github.com>
2023-11-20 16:54:04 -0500

    Removing jwt info

--- backend/service/loginService.py
+++ backend/service/loginService.py
@@ -1,7 +1,6 @@
 from data.models import Clients
 from werkzeug.security import check_password_hash
 from flask import jsonify, session
-from flask_jwt_session import create_access_token, create_refresh_token
 
 
 def loginClient(email,password):
@@ -11,6 +10,4 @@ def loginClient(email,password):
         if not check_password_hash(client.password,password):
             return {"message":"Wrong password"},401
         session["clientID"]=client.clientID
-        access_token = create_access_token(identity=client.clientID)
-        refresh_token=create_refresh_token(identity=client.clientID)
-        return jsonify({"message":"Success", "access_token":access_token, "refresh_token":refresh_token, "coachexpID":client.coachexpID})
+        return jsonify({"message":"Success", "coachexpID":client.coachexpID})
