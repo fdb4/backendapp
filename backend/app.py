a129f0db3b1c7aae17f6ddb2ee25df2a78082c08
Sandeep Singh <singh.deep8302@gmail.com>
2023-11-19 20:40:59 -0500

    jwtfix

--- backend/app.py
+++ backend/app.py
@@ -2,28 +2,38 @@ from flask import Flask
 from flask_restx import Api
 from flask_cors import CORS
 from data.exts import db
-from data.models import Clients, GeneralInfo, WorkoutGoal, CoachExp, Location, State, ClientCoaches
+from data.models import (
+    Clients,
+    GeneralInfo,
+    WorkoutGoal,
+    CoachExp,
+    Location,
+    State,
+    ClientCoaches,
+)
 from config.config import DevConfig
-from flask_jwt_sxtended import JWTManager
+from flask_jwt_extended import JWTManager
 
-app=Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 app.config.from_object(DevConfig)
 db.init_app(app)
 JWTManager(app)
-api=Api(app,doc='/docs')
+api = Api(app, doc="/docs")
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return{
-        "db":db,
-        "Clients":Clients,
+    return {
+        "db": db,
+        "Clients": Clients,
         "GeneralInfo": GeneralInfo,
         "WorkoutGoal": WorkoutGoal,
         "CoachExp": CoachExp,
         "Location": Location,
         "State": State,
-        "ClientCoaches": ClientCoaches
+        "ClientCoaches": ClientCoaches,
     }
-    
+
+
 import api
