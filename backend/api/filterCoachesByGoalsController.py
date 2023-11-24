b98dfc08779c6123593a24f75923cf4d1d952f98
saminchowdhury <sc45@njit.edu>
2023-11-24 17:53:40 -0500

    Removed Client ID and Email

--- backend/api/filterCoachesByGoalsController.py
+++ backend/api/filterCoachesByGoalsController.py
@@ -1,14 +1,12 @@
 # filterCoachesByGoalsController.py
 from flask_restx import Resource, fields
 from app import api, app
-from service.filterCoachesByStateService import filter_coaches_by_goal
+from service.filterCoachesByGoalsService import filter_coaches_by_goal
 
 
 coach_model = api.model(
    "Coaches",
    {
-       "clientID": fields.Integer(),
-       "email": fields.String(45),
        "firstname": fields.String(45),
        "lastname": fields.String(45),
        "price": fields.Float(),
