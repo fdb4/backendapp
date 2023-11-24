8d7a056d9b588bd728d7cfa1d8cbac2337f73b26
saminchowdhury <sc45@njit.edu>
2023-11-24 18:12:13 -0500

    filterCoaches

--- backend/api/filterCoachesByGoalsController.py
+++ backend/api/filterCoachesByGoalsController.py
@@ -1,29 +1,29 @@
-from flask_restx import Resource, Namespace, fields
+# filterCoachesByGoalsController.py
+from flask_restx import Resource, fields
+from app import api, app
 from service.filterCoachesByGoalsService import filter_coaches_by_goal
 
-api = Namespace('coaches', description='Coaches related operations')
+
+coach_model = api.model(
+   "Coaches",
+   {
+       "firstname": fields.String(45),
+       "lastname": fields.String(45),
+       "price": fields.Float(),
+       "rating": fields.Integer(),
+       "experience": fields.Date(),
+       "bio": fields.String(4294967295),
+       "gym": fields.String(45),
+       "town": fields.String(45),
+       "state": fields.String(45),
 
 
-coach_model = api.model('Coach', {
-    'clientID': fields.Integer(required=True, description='The client unique identifier'),
-    'email': fields.String(required=True, description='The email of the client'),
-    'firstname': fields.String(required=True, description='The first name of the client'),
-    'lastname': fields.String(required=True, description='The last name of the client'),
-    'price': fields.Float(required=True, description='The price set by the coach'),
-    'rating': fields.Integer(required=True, description='The rating of the coach'),
-    'experience': fields.Date(required=True, description='The experience start date of the coach'),
-    'bio': fields.String(required=True, description='The biography of the coach'),
-    'gym': fields.String(required=True, description='The gym where the coach works'),
-    'town': fields.String(required=True, description='The town of the gym'),
-    'state': fields.String(required=True, description='The state where the gym is located'),
-})
-
-@api.route('/filter/goal/<string:goal>')
-class CoachesByGoal(Resource):
-    @api.doc('list_coaches_by_goal')
-    @api.marshal_list_with(coach_model)
-    def get(self, goal):
-        """
-        List all coaches with a specific workout goal.
-        """
-        return filter_coaches_by_goal(goal)
+}
+  
+)
+@api.route('/coaches/filter/goal/<string:goal_type>')
+class FilterGoalResource(Resource):
+   @api.marshal_list_with(coach_model)
+   def get(self, goal_type):
+       """Filter coaches by goal"""
+       return filter_coaches_by_goal(goal_type)
