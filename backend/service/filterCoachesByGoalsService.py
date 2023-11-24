eddc7417ae047c89c5b8ee68c8c0f633b0012160
saminchowdhury <sc45@njit.edu>
2023-11-24 17:05:12 -0500

    Working filterCoachesByGoals Controller + Service

--- backend/service/filterCoachesByGoalsService.py
+++ backend/service/filterCoachesByGoalsService.py
@@ -1,38 +1,24 @@
-from data.models import Clients, CoachExp, Location, State, WorkoutGoal
+# filterCoachesByGoalsService.py
 from data.exts import db
 from sqlalchemy.sql import text
 
-def filter_coaches_by_goal(goal):
-    sql_query = text(
-        """
-        SELECT 
-            c.clientID, 
-            c.email, 
-            c.firstname, 
-            c.lastname, 
-            ce.price, 
-            ce.rating, 
-            ce.experience, 
-            ce.bio, 
-            l.gym, 
-            l.town, 
-            s.state
-        FROM 
-            clients c
-        JOIN 
-            coachexp ce ON c.coachexpID = ce.coachexpID
-        JOIN 
-            workoutgoal wg ON c.workoutgoalID = wg.workoutgoalID
-        JOIN 
-            location l ON ce.locationID = l.locationID
-        JOIN 
-            state s ON l.stateID = s.stateID
-        WHERE 
-            wg.:goal = 1
-        """
-    )
-    
 
-    query = sql_query.bindparams(goal=goal)
-    results = db.session.execute(query).fetchall()
-    return [dict(row) for row in results]
+def filter_coaches_by_goal(goal_type):
+   valid_goals = ['cycling', 'strength', 'running', 'sports', 'yoga', 'swimming', 'martialarts']
+   if goal_type not in valid_goals:
+       return []  # Return an empty list if the goal type is not valid
+
+
+   query = text(
+   "SELECT c.clientID, c.email, c.firstname, c.lastname, ce.price, ce.rating, ce.experience, ce.bio, l.gym, l.town, s.state, "
+   "wg.cycling, wg.strength, wg.running, wg.sports, wg.yoga, wg.swimming, wg.martialarts, wg.other "
+   "FROM clients c "
+   "JOIN coachexp ce ON c.coachexpID = ce.coachexpID "
+   "JOIN workoutgoal wg ON c.workoutgoalID = wg.workoutgoalID "
+   "JOIN location l ON ce.locationID = l.locationID "
+   "JOIN state s ON l.stateID = s.stateID "
+   "WHERE wg.{} = 1".format(goal_type))
+
+
+   results = db.session.execute(query).fetchall()
+   return [dict(row) for row in results]
