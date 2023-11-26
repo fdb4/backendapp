756f87df3ef05e4d7517985e0e9cc09042ff1a48
fahadali323 <afahad5312@gmail.com>
2023-11-25 21:38:39 -0500

    made changes to the signup issue with what data was being send from frontend

--- backend/app.py
+++ backend/app.py
@@ -4,13 +4,11 @@ from flask_cors import CORS
 from data.exts import db
 from data.models import Clients, GeneralInfo, WorkoutGoal, CoachExp, Location, State, ClientCoaches
 from config.config import DevConfig
-from flask_jwt_sxtended import JWTManager
 
 app=Flask(__name__)
 CORS(app)
 app.config.from_object(DevConfig)
 db.init_app(app)
-JWTManager(app)
 api=Api(app,doc='/docs')
 
 @app.shell_context_processor
