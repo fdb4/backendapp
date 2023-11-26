72132834ca6adc33c326b183c96b0c869a1897bc
saminchowdhury <sc45@njit.edu>
2023-11-26 00:03:24 -0500

    filterCoachesById Controller + Service

---
+++ backend/api/filterCoachesByIdController.py
@@ -0,0 +1,28 @@
+# filterCoachesByIDController.py
+from flask_restx import Resource, fields
+from app import api, app
+from service.filterCoachesByIdService import filter_coaches_by_id
+
+coach_model = api.model(
+    "Coach",
+    {
+        "coachexpID": fields.Integer(),
+        "price": fields.Float(),
+        "rating": fields.Integer(),
+        "experience": fields.String(),
+        "bio": fields.String(4294967295),
+        "gym": fields.String(45),
+        "town": fields.String(45),
+        "state": fields.String(45)
+    }
+)
+
+@api.route('/coaches/filter/id/<int:coach_id>')
+class FilterCoachByIDResource(Resource):
+    @api.marshal_with(coach_model)
+    def get(self, coach_id):
+        """Filter coach by ID"""
+        coach = filter_coaches_by_id(coach_id)
+        if coach:
+            return coach
+        api.abort(404, "Coach with ID {} not found".format(coach_id))
