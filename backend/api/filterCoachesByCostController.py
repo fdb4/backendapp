e6ddaf6fe7be2e862dacef130769fb3151a8d72e
saminchowdhury <sc45@njit.edu>
2023-11-25 23:34:21 -0500

    filterCoachesByCostController and Service

---
+++ backend/api/filterCoachesByCostController.py
@@ -0,0 +1,25 @@
+# filterCoachesByCostController.py
+from flask_restx import Resource, fields
+from app import api, app
+from service.filterCoachesByCostService import filter_coaches_by_cost
+
+coach_model = api.model(
+    "Coaches",
+    {
+        "coachexpID": fields.Integer(),
+        "price": fields.Float(),
+        "rating": fields.Integer(),
+        "experience": fields.String(),
+        "bio": fields.String(4294967295),
+        "gym": fields.String(45),
+        "town": fields.String(45),
+        "state": fields.String(45)
+    }
+)
+
+@api.route('/coaches/filter/cost/<float:max_price>')
+class FilterCostResource(Resource):
+    @api.marshal_list_with(coach_model)
+    def get(self, max_price):
+        """Filter coaches by cost"""
+        return filter_coaches_by_cost(max_price)
