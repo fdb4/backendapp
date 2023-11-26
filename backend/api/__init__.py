067f1b5bdb9e7b83df1dc5ec11b729c5668184dd
Gianna <gbauzil02@gmail.com>
2023-11-26 17:52:14 -0500

    View requests for a coach

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -23,3 +23,4 @@ from . import doneCoachSurveyController
 from . import clientSurveyController
 from . import clientSurveyController
 from . import dailyLogController
+from . import viewRequestsController
---
+++ backend/service/viewRequestsService.py
@@ -0,0 +1,29 @@
+from data.exts import db
+from sqlalchemy.sql import text
+
+def viewRequests(clientID):
+        query = text(
+                "select c.coachexpID "
+                "from clients c "
+                "where clientID = :cid"
+        )
+        query = query.bindparams(cid=clientID)
+        coach=db.session.execute(query).first()
+
+        coachexpID = coach.coachexpID
+        if coachexpID == None:
+            return {"message": "User is not a coach"}
+        query = text(
+                "select c.clientID, c.firstname, c.lastname "
+                "from clients c "
+                "join( "
+                "select * "
+                "from clientcoaches c "
+                "where coachexpID = :cid and request IS NULL) info "
+                "where info.clientID = c.clientID "
+        )
+
+        query = query.bindparams(cid=coachexpID)
+        requests=db.session.execute(query).fetchall()
+
+        return requests
