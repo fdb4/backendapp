09eefebf96d969dd47409ff0e66ed8f5d514a76b
saminchowdhury <sc45@njit.edu>
2023-12-04 13:25:16 -0500

    searchCoach Updated to return specialization

--- backend/api/searchCoachController.py
+++ backend/api/searchCoachController.py
@@ -2,23 +2,22 @@ from app import api, app
 from flask_restx import Resource, fields
 from service.searchCoachService import searchCoach
 
-coach_model=api.model(
+coach_model = api.model(
     "Coaches",
     {
-        "email":fields.String(45),
-        "firstname":fields.String(45),
-        "lastname":fields.String(45),
-        "price":fields.Float(),
-        "rating":fields.Integer(),
-        "experience":fields.Integer(),
-        "bio":fields.String(4294967295),
-        "gym":fields.String(45),
-        "town":fields.String(45),
-        "state":fields.String(45)
+        "email": fields.String(45),
+        "firstname": fields.String(45),
+        "lastname": fields.String(45),
+        "price": fields.Float(),
+        "rating": fields.Integer(),
+        "experience": fields.Integer(),
+        "bio": fields.String(4294967295),
+        "gym": fields.String(45),
+        "town": fields.String(45),
+        "state": fields.String(45),
+        "specializations": fields.List(fields.String)
     }
-
 )
-
 @api.route('/coaches/<int:coachexpID>')
 class CoachSearchResource(Resource):
     @api.marshal_with(coach_model)
