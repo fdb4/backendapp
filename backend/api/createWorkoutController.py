9507f8b729aa8378325fd2f990b6e6fc30f27a51
fdb4 <144566244+fdb4@users.noreply.github.com>
2023-12-06 17:24:56 -0500

    Update createWorkoutController.py

--- backend/api/createWorkoutController.py
+++ backend/api/createWorkoutController.py
@@ -2,9 +2,20 @@ from flask_restx import Resource, fields
 from flask import Flask, request
 from app import api, app
 from service.createWorkoutService import createWorkout
+womod=api.model(
+    'woModel',
+    {
+            "workoutname":fields.String(45),
+            "videolink":fields.String(300),
+            "description":fields.String(10000),
+            "musclegroup":fields.String(45),
+            "equipment":fields.String(200)
+    }
+)
 
 @api.route('/workout/create')
 class CreateWorkoutResource(Resource):
+    @api.expect(womod)
     def post(self):
         data = request.json
         createWorkout(
