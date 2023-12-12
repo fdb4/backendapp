9ef4d73158d638bf721a2c7fd84c3c726b586804
Gianna <gbauzil02@gmail.com>
2023-12-12 13:52:14 -0500

    Adding specialization to filtering

--- backend/api/filterCoachesBySpecializationController.py
+++ backend/api/filterCoachesBySpecializationController.py
@@ -17,7 +17,7 @@ coach_model = api.model(
         "gym": fields.String(45),
         "town": fields.String(45),
         "state": fields.String(45),
-        "specializations": fields.String()  
+        "specializations": fields.List(fields.String)
     }
 )
 
