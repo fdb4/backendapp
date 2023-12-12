538049c911f76c6b2b9f26aab69795076b463c0f
saminchowdhury <sc45@njit.edu>
2023-12-12 02:37:28 -0500

    editWorkoutPlanController

--- backend/api/editWorkoutPlanController.py
+++ backend/api/editWorkoutPlanController.py
@@ -1,16 +1,33 @@
-from flask_restx import Resource, reqparse
+from flask_restx import Resource, fields
+from flask import request
 from app import api
 from service.editWorkoutPlanService import edit_workout_plan
 
+# Model for exercise data
+exercise_model = api.model(
+    "ExerciseData",
+    {
+        "workoutID": fields.Integer(required=True, description="Workout ID"),
+        "Sets": fields.Integer(required=True, description="Number of sets"),
+        "reps": fields.Integer(required=True, description="Number of repetitions")
+    }
+)
+
+# Model for workout plan data
+workout_plan_model = api.model(
+    "WorkoutPlanData",
+    {
+        "planName": fields.String(required=True, description="Name of the workout plan"),
+        "exercises": fields.List(fields.Nested(exercise_model), required=True, description="List of exercises")
+    }
+)
+
 @api.route('/edit/workoutplan/<int:workoutplanID>')
 class EditWorkoutPlanResource(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('planName', required=True, type=str, help="Name of the workout plan")
-    parser.add_argument('Sets', required=True, type=int, help="Number of sets")
-    parser.add_argument('reps', required=True, type=int, help="Number of repetitions")
-    parser.add_argument('workoutID', required=True, type=int, help="ID of the workout")
-
+    @api.expect(workout_plan_model)
     def put(self, workoutplanID):
         """Edit a workout plan"""
-        data = EditWorkoutPlanResource.parser.parse_args()
-        return edit_workout_plan(workoutplanID, data)
+        data = request.json
+        planName = data['planName']
+        exercises = data['exercises']
+        return edit_workout_plan(workoutplanID, planName, exercises)
--- backend/service/editWorkoutPlanService.py
+++ backend/service/editWorkoutPlanService.py
@@ -1,20 +1,30 @@
 from data.exts import db
 from sqlalchemy.sql import text
 
-def edit_workout_plan(workoutplanID, data):
+def edit_workout_plan(workoutplanID, planName, exercises):
     try:
-        query = text("""
+        # Update planName
+        plan_name_query = text("""
             UPDATE workoutplan
-            SET planName = :planName, Sets = :Sets, reps = :reps, workoutID = :workoutID
+            SET planName = :planName
             WHERE workoutplanID = :workoutplanID
         """)
-        db.session.execute(query, {
-            'planName': data['planName'],
-            'Sets': data['Sets'],
-            'reps': data['reps'],
-            'workoutID': data['workoutID'],
-            'workoutplanID': workoutplanID
-        })
+        db.session.execute(plan_name_query, {'planName': planName, 'workoutplanID': workoutplanID})
+
+        # Update exercises
+        for exercise in exercises:
+            exercise_query = text("""
+                UPDATE workoutplan
+                SET Sets = :Sets, reps = :reps, workoutID = :workoutID
+                WHERE workoutplanID = :workoutplanID
+            """)
+            db.session.execute(exercise_query, {
+                'Sets': exercise['Sets'],
+                'reps': exercise['reps'],
+                'workoutID': exercise['workoutID'],
+                'workoutplanID': workoutplanID
+            })
+
         db.session.commit()
         return {"message": "Workout plan updated successfully"}, 200
     except Exception as e:
