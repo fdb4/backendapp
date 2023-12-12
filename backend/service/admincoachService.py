d82c1a98cc938de2626d1a5a87ca5f2a711fbd5c
Gianna <gbauzil02@gmail.com>
2023-12-11 19:19:52 -0500

    Updating reject coach

--- backend/service/admincoachService.py
+++ backend/service/admincoachService.py
@@ -1,8 +1,26 @@
 from data.models import CoachExp
 from flask import jsonify
+from sqlalchemy.sql import text
+from data.exts import db
 
 def updateCV(coachexpID,visible):#updatecoachVisibility
     coach=CoachExp.query.filter_by(coachexpID=coachexpID).first()
 
     coach.update(coach.price,coach.rating,coach.experience,coach.bio,bool(visible))
+
+    if visible == 1:
+        query = text(
+            "update clients set coachexpid = NULL, isCoach = 0 where coachexpid = :cid "
+        )
+        query = query.bindparams(cid=coachexpID)
+        db.session.execute(query)
+
+        query = text(
+            "delete from coachexp where coachexpid = :cid "
+        )
+        query = query.bindparams(cid=coachexpID)
+        db.session.execute(query)
+
+        db.session.commit()
+
     return jsonify(({"message": "coach visibility updated"}))
