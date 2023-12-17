5d518c061bfddb23f02729f88a97e3b098d4ca8b
saminchowdhury <sc45@njit.edu>
2023-12-17 00:56:51 -0500

    Updated editWorkout Plan

--- backend/api/editWorkoutPlanController.py
+++ backend/api/editWorkoutPlanController.py
@@ -1,17 +1,29 @@
 from flask_restx import Resource, fields
-from app import api
-from service.editWorkoutPlanService import update_workouts_in_plan
+from flask import request
+from app import api, app
+from service.editWorkoutPlanService import edit_workout_plan
 
-@api.route('/workoutplan/update/<int:workoutplanID>')
-class WorkoutPlanUpdateResource(Resource):
-    @api.expect(api.model('WorkoutPlanUpdate', {
-        'workouts': fields.List(fields.Nested(api.model('WorkoutUpdate', {
-            'id': fields.Integer(required=True, description='Workout Plan Entry ID'),
-            'workoutID': fields.Integer(required=True, description='Workout ID'),
-            'Sets': fields.Integer(required=True, description='Number of Sets'),
-            'reps': fields.Integer(required=True, description='Number of Reps')
-        })))
-    }))
-    def put(self, workoutplanID):
-        data = api.payload
-        return update_workouts_in_plan(workoutplanID, data['workouts'])
+workout_model = api.model(
+    'Workout',
+    {
+        "workoutID": fields.Integer(required=True, description="The workout ID"),
+        "Sets": fields.Integer(required=True, description="Number of sets"),
+        "reps": fields.Integer(required=True, description="Number of repetitions")
+    }
+)
+
+plan_model = api.model(
+    'EditWorkoutPlan',
+    {
+        "planName": fields.String(required=True, description="Name of the workout plan"),
+        "exercises": fields.List(fields.Nested(workout_model), required=True, description="List of exercises")
+    }
+)
+
+@api.route('/edit/workoutplan/<int:clientID>/<int:workoutplanID>')
+class EditWorkoutPlanResource(Resource):
+    @api.expect(plan_model)
+    def put(self, clientID, workoutplanID):
+        """Edit a workout plan"""
+        data = request.json
+        return edit_workout_plan(clientID, workoutplanID, data)
--- backend/service/editWorkoutPlanService.py
+++ backend/service/editWorkoutPlanService.py
@@ -1,24 +1,33 @@
 from data.exts import db
 from sqlalchemy.sql import text
 
-def update_workouts_in_plan(workoutplanID, workouts):
+def edit_workout_plan(clientID, workoutplanID, data):
     try:
-        for workout in workouts:
-            query = text("""
-                UPDATE workoutplan
-                SET workoutID = :workoutID, Sets = :Sets, reps = :reps
-                WHERE id = :id AND workoutplanID = :workoutplanID
+        # Delete existing workouts with the given workoutplanID for the client
+        delete_query = text("""
+            DELETE FROM workoutplan 
+            WHERE clientID = :clientID AND workoutplanID = :workoutplanID
+        """)
+        db.session.execute(delete_query, {'clientID': clientID, 'workoutplanID': workoutplanID})
+
+        # Insert new workouts
+        for exercise in data['exercises']:
+            insert_query = text("""
+                INSERT INTO workoutplan (planName, clientID, workoutplanID, workoutID, Sets, reps)
+                VALUES (:planName, :clientID, :workoutplanID, :workoutID, :Sets, :reps)
             """)
-            db.session.execute(query, {
-                'id': workout['id'],
-                'workoutID': workout['workoutID'],
-                'Sets': workout['Sets'],
-                'reps': workout['reps'],
-                'workoutplanID': workoutplanID
+            db.session.execute(insert_query, {
+                'planName': data['planName'],
+                'clientID': clientID,
+                'workoutplanID': workoutplanID,
+                'workoutID': exercise['workoutID'],
+                'Sets': exercise['Sets'],
+                'reps': exercise['reps']
             })
 
         db.session.commit()
         return {"message": "Workout plan updated successfully"}, 200
+
     except Exception as e:
         db.session.rollback()
         return {"error": str(e)}, 500
