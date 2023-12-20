061e466d50cafcf56f858176132b52a23e35ec49
fdb4 <144566244+fdb4@users.noreply.github.com>
2023-12-19 22:44:27 -0500

    Update logWorkoutPlanProgressController.py

--- backend/api/logWorkoutPlanProgressController.py
+++ backend/api/logWorkoutPlanProgressController.py
@@ -1,21 +1,21 @@
 from flask_restx import Resource, fields, reqparse
-from app import api
+from app import api2
 from service.logWorkoutPlanProgressService import log_workout_progress
 
-log_model = api.model('LogModel', {
+log_model = api2.model('LogModel', {
     "clientID": fields.Integer(required=True, description="Client ID"),
     "workoutplanID": fields.Integer(required=True, description="Workout Plan ID"),
-    "exerciseLogs": fields.List(fields.Nested(api.model('ExerciseLog', {
+    "exerciseLogs": fields.List(fields.Nested(api2.model('ExerciseLog', {
         "workoutID": fields.Integer(required=True, description="Workout ID"),
         "sets": fields.Integer(required=True, description="Number of sets"),
         "reps": fields.Integer(required=True, description="Number of repetitions")
     })), required=True, description="List of exercises with sets and reps")
 })
 
-@api.route('/log/workoutprogress')
+@api2.route('/log/workoutprogress')
 class LogWorkoutProgressResource(Resource):
-    @api.expect(log_model)
+    @api2.expect(log_model)
     def post(self):
         """Log workout progress for a client"""
-        data = api.payload
+        data = api2.payload
         return log_workout_progress(data)
