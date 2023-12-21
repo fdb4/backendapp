dd7e8e2336d1bb36617848b1ff73f269898588ab
fdb4 <144566244+fdb4@users.noreply.github.com>
2023-12-20 22:18:53 -0500

    Update getWorkoutPlanLogController.py

--- backend/api/getWorkoutPlanLogController.py
+++ backend/api/getWorkoutPlanLogController.py
@@ -1,8 +1,8 @@
 from flask_restx import Api, Resource, fields
-from app import api, app
+from app import api2, app
 from service.getWorkoutPlanLogService import get_client_workout_logs
 
-workout_log_model = api.model(
+workout_log_model = api2.model(
     "WorkoutLog",
     {
         "planName": fields.String(),
@@ -15,9 +15,9 @@ workout_log_model = api.model(
     }
 )
 
-@api.route('/workoutlogs/client/<int:client_id>')
+@api2.route('/workoutlogs/client/<int:client_id>')
 class ClientWorkoutLogsResource(Resource):
-    @api.marshal_list_with(workout_log_model)
+    @api2.marshal_list_with(workout_log_model)
     def get(self, client_id):
         """Retrieve workout log progress for a specific client"""
         return get_client_workout_logs(client_id)
